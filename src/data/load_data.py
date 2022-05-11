fafd14c1e25b36e29c57bf7e942c077ca509cb93
sounritesh <sounritesh@gmail.com>
2022-05-11 10:57:35 +0530

    initial commit

---
+++ src/data/load_data.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+from tqdm import tqdm
+import pandas as pd
+
+from src.config.secrets import secrets
+
+def load_data_for_tm():
+    client = MongoClient(secrets["client"])
+
+    thought_list = []
+    thoughts = client.forum.thoughts
+    for thought in tqdm(thoughts.find(no_cursor_timeout=True), total=thoughts.count_documents({})):
+        is_top_level = 0 if 'c_parent' in thought and not not thought['c_parent'] else 1
+        if is_top_level:
+            thought_list.append(thought)
+    print("{} thoughts retrieved from client.".format(len(thought_list)))
+    df_thoughts = pd.DataFrame(thought_list)[["_id", "body", "author"]]
+
+    return df_thoughts
---
+++ topic_modelling.py
@@ -0,0 +1,45 @@
+from src.data.load_data import load_data_for_tm
+from src.config.config import logger
+
+from bertopic import BERTopic
+from argparse import ArgumentParser
+import os
+import time
+
+parser = ArgumentParser()
+
+parser.add_argument("--output_dir", type=str, default="")
+args = parser.parse_args()
+
+def main():
+    st = time.time()
+    logger.info("Loading data from mongo db...")
+    df = load_data_for_tm()
+    logger.info("Data Loaded (took {} seconds).".format(time.time() - st))
+
+    text_list = df.body.tolist()
+
+    st = time.time()
+    logger.info("Initializing model...")
+    topic_model = BERTopic(language="english", calculate_probabilities=True, verbose=True)
+    logger.info("Model initialized (took {} seconds).".format(time.time() - st))
+    
+    st = time.time()
+    logger.info("Fitting model...")
+    topics, probs = topic_model.fit_transform(text_list)
+    logger.info("Model fit successful (took {} seconds).".format(time.time() - st))
+
+    st = time.time()
+    logger.info("Saving Model to {}...".format(args.output_dir))
+    topic_model.save(os.path.join(args.output_dir, "topic_model"))
+    logger.info("Model saved (took {} seconds).".format(time.time() - st))
+    
+    st = time.time()
+    logger.info("Saving output to {}...".format(args.output_dir))
+    df["topic"] = topics
+    df.to_csv(os.path.join(args.output_dir, "modeled_thoughts.csv"), index=False)
+    logger.info("Output saved (took {} seconds).".format(time.time() - st))
+    logger.info("Done!")
+
+if __name__=="__main__":
+    main()
