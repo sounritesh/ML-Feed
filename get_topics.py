3b0d6db8f255e8db40fdfc93a1da96d7838fc45b
Ritesh Soun <sounritesh@gmail.com>
2022-05-13 22:57:29 +0530

    topic retrieval code

---
+++ get_topics.py
@@ -0,0 +1,36 @@
+from ast import arg
+from email import parser
+from email.policy import default
+from bertopic import BERTopic
+from argparse import ArgumentParserr
+import os
+import pandas as pd
+from tqdm import tqdm
+
+parser = ArgumentParserr()
+parser.add_argument("--dir", default="", type=str)
+
+args = parser.parse_args()
+
+def main():
+    topic_model = BERTopic.load(os.path.join(args.dir, "topic_model"))
+
+    topic_list = []
+
+    for i in tqdm(range(250)):
+        topic = topic_model.get_topic(i)
+        topic_list.append(
+            " ".join([j for j, k in topic])
+        )
+
+    df = pd.DataFrame(
+        {
+            "topic": topic_list
+        }
+    )
+
+    df.to_csv(os.path.join(args.dir, "topic_map.csv"), index=False)
+
+
+if __name__ == "__main__":
+    main()
